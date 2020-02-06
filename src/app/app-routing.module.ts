12b62ebe18891c860ff1334583d7a4f8a859bbb0
Rusty Jonas Gallardo <rusty.gallardo@avannubo.com>
2020-02-06 09:14:57 +0100

    Setting Up Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,10 @@ import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 const appRoutes: Routes = [
+  { path: '', redirectTo: '/recipes', pathMatch: 'full'},
   { path: 'recipes', component: RecipesComponent},
   { path: 'shopping-list', component: ShoppingListComponent},
 ];
-
-
 
 @NgModule({
   imports: [RouterModule.forRoot(appRoutes)],
